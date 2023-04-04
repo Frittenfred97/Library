8419ea092abe35c5f422f4a71baac9ed9e744331
Frederic Carboneras <carboneras.frederic@gmail.com>
2023-04-04 19:17:10 +0200

    added function for Modal form
    - added form input to console
    - card design rework

--- app.js
+++ app.js
@@ -2,25 +2,30 @@ const divRow = document.getElementById("books");
 
 let myLibrary = [
     {
-        "title": "Der Marsianer",
+        "title": "The Martian",
         "author": "Andy Weir",
-        "pages": "381"
+        "pages": "381",
+        "informations": "Good Sci-Fi Book!"
     },
 ];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, informations) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.informations = informations;
+
+    Book.push
 };
 
 function addBookToLibrary() {
     for (let i = 0; i < myLibrary.length; i++) {
 
         const divCol = document.createElement("div");
-        divCol.className = "col-3";
+        divCol.className = "col-auto";
         const divCard = document.createElement("div");
-        divCard.className = "card";
+        divCard.className = "card shadow";
+        divCard.setAttribute("Data-tilt", "");
         const divCardHeader = document.createElement("div");
         divCardHeader.className = "card-header";
         const cardTitle = document.createElement("h3");
@@ -36,19 +41,25 @@ function addBookToLibrary() {
         titleNumbers.innerText = "Page numbers:";
         const pageNumbers = document.createElement("p");
         pageNumbers.innerText = myLibrary[i].pages;
+        const titleInformations = document.createElement("h3");
+        titleInformations.innerText = "Additional information:" 
+        const bookInformations = document.createElement("p");
+        bookInformations.className = "text-muted";
+        bookInformations.innerText = myLibrary[i].informations;
         const divCardFooter = document.createElement("div");
-        divCardFooter.className = "card-footer";
+        divCardFooter.className = "card-footer d-flex align-items-center justify-content-between";
         const labelForm = document.createElement("label");
-        labelForm.className = "form-check form-switch";
+        labelForm.className = "form-check form-switch col-6";
         const inputForm = document.createElement("input");
         inputForm.className = "form-check-input";
         inputForm.type = "checkbox";
         const spanForm = document.createElement("span");
         spanForm.className = "form-check-label"
-        spanForm.innerText = "read/unread";
+        spanForm.innerText = "unread/read";
         const deleteButton = document.createElement("button");
-        deleteButton.className = "btn";
-        deleteButton.innerText = "Delete";
+        deleteButton.className = "btn btn-danger";
+        const deleteImage = document.createElement("img");
+        deleteImage.src = "images/trash-x.svg"
 
 
         divRow.append(divCol);
@@ -60,38 +71,36 @@ function addBookToLibrary() {
         divCardBody.append(bookAuthor);
         divCardBody.append(titleNumbers);
         divCardBody.append(pageNumbers);
+        divCardBody.append(titleInformations);
+        divCardBody.append(bookInformations);
         divCard.append(divCardFooter);
         divCardFooter.append(labelForm);
         labelForm.append(inputForm);
         labelForm.append(spanForm);
         divCardFooter.append(deleteButton);
+        deleteButton.append(deleteImage);
     }
 };
 
+function getFormValues() {
+    const getTitle = document.getElementById("title").value;
+    const getAuthor = document.getElementById("author").value;
+    const getNumber = document.getElementById("number").value;
+    const getInformation = document.getElementById("information").value;
+
+    Book(getTitle, getAuthor, getNumber, getInformation);
+
+    myLibrary.push(Book);
+
+    console.log(getTitle);
+    console.log(getAuthor);
+    console.log(getNumber);
+    console.log(getInformation);
+
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("number").value = "";
+    document.getElementById("information").value = "";
+}
+
 addBookToLibrary();
-
-/* 
-                <div class="col-3">
-                    <div class="card">
-                        <div class="card-header">
-                          <h3 class="card-title mx-auto">Der Marsianer</h3>
-                        </div>
-                        <div class="card-body">
-                            <h3>Autor:</h3>
-                            <p>Andy Weir</p>
-                            <h3>Seitenanzahl:</h3>
-                            <p>381</p>
-                        </div>
-                        <div class="card-footer">
-                            <label class="form-check form-switch">
-                                <input class="form-check-input" type="checkbox" >
-                                <span class="form-check-label">Read/unread</span>
-                              </label>
-                              <button class="btn">
-                                Delete
-                              </button>
-                        </div>
-                      </div>
-                </div>
-
-*/
